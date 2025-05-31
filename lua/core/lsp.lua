b08c98f6d130fc40bb6ed7423ec93f20947b7f66
Adib Hanna <adibhanna@gmail.com>
2025-05-31 18:09:10 -0500

    wip

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -4,8 +4,8 @@ vim.lsp.enable({
 })
 
 vim.diagnostic.config({
-    -- virtual_lines = true,
-    virtual_text = true,
+    virtual_lines = true,
+    -- virtual_text = true,
     underline = true,
     update_in_insert = false,
     severity_sort = true,
