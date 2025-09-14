2362c27585cda1e7684885379b962374b77ccea4
Prasanna <prassee@prassee.me>
2025-09-14 19:16:12 +0530

    added shortcut to trigger lazygit

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -12,11 +12,10 @@ return {
       hidden = true,
     })
 
-    function _LAZYGIT_TOGGLE()
+    vim.keymap.set("n", "<leader>z", function()
       lazygit:toggle()
-    end
-    --    vim.api.nvim_set_keymap("n", "<leader>ng", ":Neogit<CR>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<leader>lg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", { desc = "Toggle Lazygit" })
+    end, { desc = "Toggle Lazygit" })
+
     vim.api.nvim_set_keymap(
       "n",
       "<leader>t",
@@ -33,12 +32,6 @@ return {
       "n",
       "<leader>_",
       ":ToggleTerm direction=horizontal<CR>",
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "v",
-      "<leader><leader><Enter>",
-      ":ToggleTermSendVisualSelection <CR>",
       { noremap = true, silent = true }
     )
   end,
