4dc0ae9080e240680f577ecce6e06d3a96fd44ac
Prasanna <prassee@prassee.me>
2025-10-04 18:36:35 +0530

    updated with new folder convention

--- init.lua
+++ init.lua
@@ -1,5 +1,30 @@
--- vim.loader.enable()
+-- Leader keys (must be before plugins)
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
 
-require("config.options")
-require("config.lazy")
-require("config.keymaps")
+-- Load core settings
+require("core.options")
+require("core.keymaps")
+require("core.autocmds")
+
+-- Bootstrap lazy.nvim and plugins
+-- require("plugins")
+--
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+-- Load all plugins
+require("lazy").setup({
+  { import = "plugins" },
+})
---
+++ lua/core/options.lua
@@ -0,0 +1,33 @@
+local opt = vim.opt
+
+opt.mouse = "a"
+opt.showmode = true
+vim.schedule(function()
+  opt.clipboard = "unnamedplus"
+end)
+opt.breakindent = true
+opt.undofile = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.signcolumn = "yes"
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.splitright = true
+opt.splitbelow = true
+opt.list = false
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+opt.inccommand = "split"
+opt.cursorline = true
+opt.scrolloff = 10
+opt.swapfile = false
+opt.backup = false
+
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.winborder = "rounded" -- https://neovim.io/doc/user/options.html#'winborder'
+-- disable swap file
+vim.opt.swapfile = false
+-- disable backup file
+vim.opt.backup = false
+
+vim.g.copilot_no_tab_map = true
