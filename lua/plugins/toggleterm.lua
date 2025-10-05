87a858d97a868719f726c50865466cb7422b616c
Prasanna <prassee@prassee.me>
2025-10-05 05:42:55 +0530

    Add opencode.nvim plugin and switch to gruvbox theme

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,18 +5,20 @@ return {
 
     local Terminal = require("toggleterm.terminal").Terminal
     local lazygit = Terminal:new({ cmd = "lazygit", direction = "float", hidden = true })
-    local opencode = Terminal:new({ cmd = "opencode", direction = "vertical", hidden = true })
+    local opencode = Terminal:new({ cmd = "opencode", direction = "float", hidden = true })
 
     vim.keymap.set("n", "<leader>g", function()
       lazygit:toggle()
     end, { desc = "Lazygit" })
+
     vim.keymap.set(
       "n",
       "<leader>t",
       ":ToggleTerm direction=float<CR>",
       { noremap = true, silent = true }
     )
-    vim.keymap.set("n", "<leader>o", function()
+
+    vim.keymap.set("n", "<leader>O", function()
       opencode:toggle()
     end, { desc = "OpenCode" })
   end,
