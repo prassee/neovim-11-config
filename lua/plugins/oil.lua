690824ab768a3003a28e78194389f33be9e3e416
Prasanna <prassee@prassee.me>
2025-10-05 07:23:10 +0530

    Switch to catppuccin theme and fix plugin indentation

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,21 +2,21 @@ return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-
-require("oil").setup({
-  keymaps = {
-    ["gd"] = {
-      desc = "Toggle details",
-      callback = function()
-        local detail = not detail
-        if detail then
-          require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
-        else
-          require("oil").set_columns({ "icon" })
-        end
-      end,
-    },
-  },
-})
+    require("oil").setup({
+      keymaps = {
+        ["gd"] = {
+          desc = "Toggle details",
+          callback = function()
+            local detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
+      },
+    })
   end,
 }
+
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,27 +1,20 @@
---[[ return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      -- transparent = true,
-      -- styles = { sidebars = "transparent", floats = "transparent" },
-    })
-    vim.cmd.colorscheme("tokyonight-storm")
-  end,
-} ]]
-
 return {
-  "ellisonleao/gruvbox.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
   config = function()
-    require("gruvbox").setup({
-      contrast = "soft", -- can be "hard", "soft" or empty string
-      overrides = {
-        Normal = { bg = "#1d2021" },
-        NormalFloat = { bg = "#1d2021" },
-        FloatBorder = { bg = "#1d2021", fg = "#458588" },
+    require("catppuccin").setup({
+      flavour = "frappe",
+      transparent_background = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
       },
     })
-    vim.cmd.colorscheme("gruvbox")
+    vim.cmd.colorscheme("catppuccin")
   end,
 }
