8e166ba4c50eaec1659447b4a8d9824b714981f4
Prasanna <Prasanna.kumar@lendingkart.com>
2025-10-15 15:09:30 +0530

    updated

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,18 @@
 return {
   "stevearc/conform.nvim",
-config = function()
-require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    go = { "goimports", "golines", "gofmt" },
-    python = { "ruff", "black" },
-    toml = { "taplo" },
-    json = { "prettier" },
-    jsonc = { "prettier" },
-    markdown = { "prettier" },
-  },
-  format_on_save = { lsp_fallback = true },
-})
-end,
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        go = { "goimports", "golines", "gofmt" },
+        python = { "ruff", "black" },
+        toml = { "taplo" },
+        json = { "prettier" },
+        jsonc = { "prettier" },
+        markdown = { "prettier" },
+      },
+      format_on_save = { lsp_fallback = true },
+    })
+  end,
 }
+
